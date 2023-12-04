2a82dafb23192cf02d8444a8ddfb72cb9bb034ce
Lokesh Madupuru <lokeshmadupuru@Lokeshs-MacBook-Air.local>
2023-12-04 11:52:21 -0600

    Initial commit02

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuemXWWlWGFv87dEFFrB7YPRWosBSaOoA",
-  authDomain: "signinusingfirebase-85e80.firebaseapp.com",
-  projectId: "signinusingfirebase-85e80",
-  storageBucket: "signinusingfirebase-85e80.appspot.com",
-  messagingSenderId: "1027686134203",
-  appId: "1:1027686134203:web:3f5b0671a8aa6c2c70d49e",
-  measurementId: "G-D40QKGXFW5",
+  apiKey: "YOUR-APIKEY",
+  authDomain: "YOUR-AUTHDOMAIN",
+  projectId: "YOUR-PROJECTID",
+  storageBucket: "YOUR-STORAGEBUCKET",
+  messagingSenderId: "YOUR-MESSAGINGSENDERID",
+  appId: "YOUR-APPID",
 };
 
 // Initialize Firebase
